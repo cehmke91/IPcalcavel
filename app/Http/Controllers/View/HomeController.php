bd6e2d335633143d8d6d6965c4a9cdac1deb88d7
Caius Ehmke <cehmke91@gmail.com>
2023-01-16 20:59:34 +0100

    Routing to use controllers for views

---
+++ app/Http/Controllers/View/HomeController.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\View;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\View;
+
+/** Render out the home page. */
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+        return View::make('pages/home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\View\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return View::make('pages/home');
-});
+Route::get('/', HomeController::class);
